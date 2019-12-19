9e2c1231ccd00f1eb8daabad79871300b7744210
Pegleg <pegleg@linux.com>
2019-12-18 22:21:44 -0800

    Refactor to minimal example

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,13 @@
 require("./Utils/Extensions.js");
-import { writeFile, readFile, jsonRequest, timeNow } from "./Utils/NodeUtils";
+import { writeFile, readFile, timeNow } from "./Utils/NodeUtils";
 import { OneCompiler } from "./OneCompiler";
-import { langConfigs, LangConfig, CompileResult } from "./Generator/LangConfigs";
-import { LangFileSchema } from "./Generator/LangFileSchema";
-import { deindent } from "./Generator/Utils";
-import { TypeScriptParser2 } from "./Parsers/TypeScriptParser2";
+import { langConfigs} from "./Generator/LangConfigs";
 const fs = require("fs");
 global["YAML"] = require('yamljs'); 
-declare var YAML;
 
 global["debugOn"] = false;
 
 let prgNames = ["all"];
-const runPrg = false;
 const langFilter = "";
 const compileAll = prgNames[0] === "all";
 
@@ -32,51 +27,20 @@ for (const langConfig of langConfigVals) {
     langConfig.schema = OneCompiler.parseLangSchema(langYaml, compiler.stdlibCtx.schema);
 }
 
-for (const prgName of prgNames) {
-    console.log(`converting program '${prgName}'...`);
-    compiler.saveSchemaStateCallback = (type: "overviewText"|"schemaJson", schemaType: "program"|"overlay"|"stdlib", name: string, data: string) => {
-        writeFile(`generated/${schemaType === "program" ? prgName : schemaType}/schemaStates/${name}.${type === "overviewText" ? "txt" : "json"}`, data); 
-    };
-
-    const programCode = readFile(`input/${prgName}.ts`).replace(/\r\n/g, '\n');
+    const programCode = readFile(`input/Pegleg.ts`).replace(/\r\n/g, '\n');
     let t0 = timeNow();
     compiler.parse("typescript", programCode);
-    const parseTime = timeNow() - t0;
     
     const compileTimes = [];
     for (const lang of langConfigVals) {
         if (langFilter && lang.name !== langFilter) continue;
-    
-        //console.log(`converting program '${prgName}' to ${lang.name}...`);
         const ts = [];
-        //try {
-            t0 = timeNow();
-            const codeGen = compiler.getCodeGenerator(lang.schema);
-            lang.request.code = codeGen.generate(true);
-            compileTimes.push(timeNow() - t0);
-            writeFile(`generated/${prgName}/results/${prgName}.${codeGen.lang.extension}`, codeGen.generatedCode);
-        //} catch(e) {
-        //    console.error(e);
-        //}
-    }
+        t0 = timeNow();
+        const codeGen = compiler.getCodeGenerator(lang.schema);
+        lang.request.code = codeGen.generate(true);
+        compileTimes.push(timeNow() - t0);
+        console.log(codeGen.generatedCode);
 
-    console.log(`parse time: ${parseTime}ms, compile time: ${compileTimes.reduce((x, y) => x + y, 0)}ms (${compileTimes.join("+")})`);
     
-    // run compiled codes
-    async function executeCodes() {
-        console.log(" === START === ");
-        var promises = langConfigVals.map(async lang => {
-            if (langFilter && lang.name !== langFilter) return true;
-    
-            const result = await jsonRequest<CompileResult>(`http://127.0.0.1:8000/compile`, lang.request);
-            console.log(`${lang.name}: ${JSON.stringify(result.result||result.exceptionText||"?")}`);
-            return true;
-        });
-        const results = await Promise.all(promises);
-        console.log(" === DONE === ", results.every(x => x));
-    }
-    
-    if (runPrg && !compileAll)
-        executeCodes();
 }
 
